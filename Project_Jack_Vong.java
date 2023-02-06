fc01ee987815d95e078fa61fcd61906655fafece
Yokidoh <115113136+Yokidoh@users.noreply.github.com>
2023-02-05 21:16:11 -0500

    Create Project_Jack_Vong.java

---
+++ Project_Jack_Vong.java
@@ -0,0 +1,17 @@
+//Frist project Java 251
+public class Project_Jack_Vong
+{
+   public static void main(String[]args)
+   {
+           System.out.println("Hello World!");
+         
+         String name = "George Washington";
+         
+         System.out.println(name);
+         
+         int age = 24;
+         
+         System.out.println(age);
+         
+          }
+}
